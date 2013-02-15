8b0a7bbf22f5179c791de8a9965d99ff2e9f1fbf
Herat <hag59@cornell.edu>
2013-02-15 15:38:08 -0500

    xml parser error

--- ngram.py
+++ ngram.py
@@ -2,6 +2,7 @@
 Author: Herat Gandhi
 '''
 import re
+from bs4 import BeautifulSoup
 '''
 file operations
 param: array of file names
@@ -9,14 +10,41 @@ param: array of file names
 def file_operations(filenames):
     # process each file individually
     for filename in filenames:
+        
+        #detect corpus so that information can be used for parsing
+        if filename.find('wsj') != -1:
+            case = 1
+        elif filename.find('set3') != -1:
+            case = 2
+        elif filename.find('set4') != -1:
+            case = 3
+        else:
+            case = 4    
+        
+        #Get only text
+        if case in [1,2,3]:
+            xmldoc = BeautifulSoup(filename)
+            if case == 1:
+                tagName = 'TEXT'
+            elif case == 2:
+                tagName = 'DOC'
+            elif case == 3:
+                tagName = 'DOC'
+            itemlist = xmldoc.getElementsByTagName(tagName)
+            #print (itemlist[0].value)
+        '''
         #open file in read mode
         lines = open(filename, 'r').read()
         sentences = re.compile(r'(?<=[.!?;])\s*').split(lines)
         sentences_with_tag = '';
         for sentence in sentences:
-            sentences_with_tag += '<s> '+sentence+' </s>'
+            sentences_with_tag += ' <s> '+sentence+' </s> '
+        words = list(set(sentences_with_tag.split()))
+        #print(len(words))
+        print(words)'''
+        
 
 def main():
-    file_operations(['wsj/wsj.test'])
+    file_operations(['wsj/wsj.train'])
         
 main()
