d98fec539a9f303038c59646b3f68dadd7835394
Herat <hag59@cornell.edu>
2013-02-15 18:11:30 -0500

    Count of tokens

--- ngram.py
+++ ngram.py
@@ -2,7 +2,13 @@
 Author: Herat Gandhi
 '''
 import re
-from bs4 import BeautifulSoup
+
+def dupli(the_list):
+    the_m_list = the_list.split()
+    count = the_m_list.count
+    result = [(item, count(item)) for item in set(the_m_list)]
+    return result
+
 '''
 file operations
 param: array of file names
@@ -10,38 +16,13 @@ param: array of file names
 def file_operations(filenames):
     # process each file individually
     for filename in filenames:
-        
-        #detect corpus so that information can be used for parsing
-        if filename.find('wsj') != -1:
-            case = 1
-        elif filename.find('set3') != -1:
-            case = 2
-        elif filename.find('set4') != -1:
-            case = 3
-        else:
-            case = 4    
-        
-        #Get only text
-        if case in [1,2,3]:
-            xmldoc = BeautifulSoup(filename)
-            if case == 1:
-                tagName = 'TEXT'
-            elif case == 2:
-                tagName = 'DOC'
-            elif case == 3:
-                tagName = 'DOC'
-            itemlist = xmldoc.getElementsByTagName(tagName)
-            #print (itemlist[0].value)
-        '''
         #open file in read mode
         lines = open(filename, 'r').read()
         sentences = re.compile(r'(?<=[.!?;])\s*').split(lines)
         sentences_with_tag = '';
         for sentence in sentences:
             sentences_with_tag += ' <s> '+sentence+' </s> '
-        words = list(set(sentences_with_tag.split()))
-        #print(len(words))
-        print(words)'''
+        print (dupli(sentences_with_tag))
         
 
 def main():
