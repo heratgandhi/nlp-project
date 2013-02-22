115bf07b6c03efa4039e204233e7db44173b5401
Herat <hag59@cornell.edu>
2013-02-22 14:01:29 -0500

    bigram sentence generator

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def addOneSmoothingBigram(unigram_dict, probability_dict, vocab_length):
         add_one_smooth_bi[key] = float(value + 1.0) / (float(unigram_dict[key.split()[0]]) + vocab_length)
     return add_one_smooth_bi
 
-def randomeSentenceGenerator(unigram_dict,text,vocab_length,model):
+def randomSentenceGenerator(unigram_dict,text,vocab_length,bigram_prob,model):
     sentence = ''
     max_prob = unigram_dict[max(unigram_dict, key = lambda x: unigram_dict.get(x) )]
-    print(max_prob)
+    
     if model == 1:
         print('unigram model for sentence generation')
         while (not '</s>' in sentence):
@@ -35,6 +35,21 @@ def randomeSentenceGenerator(unigram_dict,text,vocab_length,model):
                     sentence += ' ' + key
     else:
         print('bigram model for sentence generation')
+        while (not '</s>' in sentence):
+            if len(sentence) > 30:
+                break
+            random_p = random.uniform(0,1)
+            if not '<s>' in sentence:
+                for key,value in bigram_prob.items():
+                    if ('<s>' == key.split()[0]) and (value >= random_p - 0.05 and value <= random_p + 0.05) :
+                        sentence += key
+                        break
+            else:
+                lastword = sentence.split()[-1]
+                for key,value in bigram_prob.items():
+                    if (lastword == key.split()[0]) and (value >= random_p - 0.05 and value <= random_p + 0.05) :
+                        sentence += ' ' + key.split()[1]
+                        break
     sentence = re.sub('<[^<]+>', "", sentence)
     return sentence
         
@@ -42,12 +57,14 @@ def probfinder(sentence,text,unigram_dict,perplexity,smoothing,vocab_length):
     if not perplexity:
         sentence = '<s> ' + sentence + ' </s>'
     words.extend(sentence.split())
+    
     index = 0
     while index < len(words)-1:
         words[index] += ' ' + words[index+1]
         index += 1
     words.pop()
     prob = 1
+    
     for word in words:
         if word.split()[0] in unigram_dict.keys():
             if smoothing:
@@ -75,11 +92,8 @@ def file_operations(filenames):
         for sentence in sentences:
             sentences_with_tag += ' <s> '+sentence+' </s> '
     words = sentences_with_tag.split()
-    
-    print(words)
-    print(sentences_with_tag)    
     unigram_dict = dict(Counter(words).items())
-    print(unigram_dict)
+    
     len_unigram = len(unigram_dict)
      
     unigram_prob = dict(unigram_dict)
@@ -103,11 +117,27 @@ def file_operations(filenames):
         bigram_prob[key] = value / float(unigram_dict[key1])
          
     bigram_prob_smooth_1 = addOneSmoothingBigram(unigram_dict, bigram_dict, len_unigram)'''
+    index_r = 0
+    bi_words = words
+    while index_r < len(bi_words):
+        if index_r + 1 < len(bi_words):
+            bi_words[index_r] += " " + bi_words[index_r+1]
+        index_r += 1
+        
+    bigram_dict = dict(Counter(bi_words).items())
+    len_bigram = len(bigram_dict)
+    bigram_prob = dict(bigram_dict)
+    for key,value in bigram_prob.items():
+        key1 = key.split()[0]
+        bigram_prob[key] = value / float(unigram_dict[key1])
+    
     #print(probfinder('There is a bug', sentences_with_tag, unigram_dict,False,False,len_unigram))
     unigram_prob = dict(sorted(unigram_prob.items(), key=itemgetter(1),reverse = True))
-    print(randomeSentenceGenerator(unigram_prob, sentences_with_tag, len_unigram, 1))
+    
+    for i in range(5):
+        print(str(i+1) + " " + randomSentenceGenerator(unigram_prob, sentences_with_tag, len_unigram, bigram_prob,2))
 
 def main():
-    file_operations(['small.txt','small2.txt'])
+    file_operations(['wsj/wsj.train'])
         
 main()
