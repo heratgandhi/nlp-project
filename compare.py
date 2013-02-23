f3d91bbd372f4c7132c48daa105a8f8b64b3979d
Herat <hag59@cornell.edu>
2013-02-22 21:58:45 -0500

    compare validation set with answers

--- compare.py
+++ compare.py
@@ -4,7 +4,7 @@ f1 = open(filenames[0], 'r')
 f = open(filenames[1],'r')
 for line in f:
     line1 = line.split()[0]
-    line2 = f1.readline().split()[0]
+    line2 = f1.readline().rstrip('\n')
     if line1 == line2:
         print("Same "+line1)
     else:
